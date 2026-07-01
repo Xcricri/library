9d4a193f9b2da9e806f06dd456ca0960afeca998
Fahcri Muhammad <fahcrimnurilham@gmail.com>
2026-07-01 16:15:16 +0700

    style: implement application layout structure

--- resources/views/layouts/app/navbar.blade.php
+++ resources/views/layouts/app/navbar.blade.php
@@ -11,26 +11,29 @@
 <body>
     <header>
         @if (Route::has('login'))
-            <nav class="flex items-center justify-end gap-4">
+            <flux:navbar>
                 @auth
-                    <a href="{{ route('dashboard') }}"
-                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                    <flux:navbar.item href="{{ route('dashboard') }}"
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                        wire:navigate>
                         Dashboard
-                    </a>
+                    </flux:navbar.item>
                 @else
-                    <a href="{{ route('login') }}"
-                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal">
+                    <flux:navbar.item href="{{ route('login') }}"
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                        wire:navigate>
                         Log in
-                    </a>
+                    </flux:navbar.item>
 
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                        <flux:navbar.item href="{{ route('register') }}"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                            wire:navigate>
                             Register
-                        </a>
+                        </flux:navbar.item>
                     @endif
                 @endauth
-            </nav>
+            </flux:navbar>
         @endif
     </header>
     {{ $slot }}
