7000ea234282098cd452c15377fa619fe66ea76e
Fahcri Muhammad <fahcrimnurilham@gmail.com>
2026-07-04 19:20:06 +0700

    refactor: update web route configuration

--- routes/web.php
+++ routes/web.php
@@ -8,25 +8,25 @@ Route::view('/', 'welcome')->name('home');
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::view('admin/dashboard', 'dashboard-admin')->name('admin.dashboard');
 
-    Route::prefix('users')->name('users.')->group(function () {
+    Route::prefix('admin/users')->name('users.')->group(function () {
         Route::livewire('/create', 'pages::users.create')->name('create');
         Route::livewire('/edit/{id}', 'pages::users.edit')->name('edit');
         Route::livewire('/index', 'pages::users.index')->name('index');
     });
 
-    Route::prefix('categories')->name('categories.')->group(function () {
+    Route::prefix('admin/categories')->name('categories.')->group(function () {
         Route::livewire('/create', 'pages::categories.create')->name('create');
         Route::livewire('/edit/{id}', 'pages::categories.update')->name('update');
         Route::livewire('/index', 'pages::categories.index')->name('index');
     });
 
-    Route::prefix('genres')->name('genres.')->group(function () {
+    Route::prefix('admin/genres')->name('genres.')->group(function () {
         Route::livewire('/create', 'pages::genres.create')->name('create');
         Route::livewire('/edit/{id}', 'pages::genres.update')->name('update');
         Route::livewire('/index', 'pages::genres.index')->name('index');
     });
 
-    Route::prefix('books')->name('books.')->group(function () {
+    Route::prefix('admin/books')->name('books.')->group(function () {
         Route::livewire('/create', 'pages::books.create')->name('create');
         Route::livewire('/edit/{id}', 'pages::books.update')->name('update');
         Route::livewire('/index', 'pages::books.index')->name('index');
